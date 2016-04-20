f3f38628af0318da59e6eabd176d6bef6aadadde
derzorngottes <amy.conchie@gmail.com>
2016-04-20 14:42:19 -0700

    Replace jade files with handlebars files

--- app.js
+++ app.js
@@ -37,7 +37,7 @@ passport.use(new MeetupOAuth2Strategy ({
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set('view engine', 'hbs');
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
