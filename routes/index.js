f3507a000fca10d6c64aa4fca2b75a005e218738
alexander "sandy" gilfillan <alexander.l.gilfillan@gmail.com>
2016-04-20 11:57:27 -0700

    beinning to modularize the rp uri

--- routes/index.js
+++ routes/index.js
@@ -8,9 +8,11 @@ var userLat;
 var userLon;
 var key = '7731a58403d7c1d1d331c3e714c349';
 var markers = [];
-var venueName;
 
-rp({uri:'https://api.meetup.com/2/open_events?key='+key +'&zip='+userZip.toString()+'&radius='+radius.toString()+'&status=upcoming'
+var r = '&radius=' + radius.toString();
+var z = '&zip=' + userZip.toString();
+
+rp({uri:'https://api.meetup.com/2/open_events?key='+key+z+r+'&status=upcoming'
 }).then(function(data) {
   var parseData = (JSON.parse(data));
   for(var i = 0; i < parseData.results.length; i++) {
@@ -19,14 +21,18 @@ rp({uri:'https://api.meetup.com/2/open_events?key='+key +'&zip='+userZip.toStrin
       markers.push({
         eventId: marker.id,
         eventName: marker.name,
+        eventUrl: marker.event_url,
         fee: marker.fee,
         venueName: marker.venue.name,
         rsvpCount: marker.yes_rsvp_count,
+        rsvpLimit: marker.rsvp_limit,
         lat: marker.venue.lat,
         lon: marker.venue.lon,
         venuePhone: marker.venue.phone,
         description: marker.description,
+        // groupPhoto: marker.group,
       })
+
     }
   }
   console.log(markers);
