7cb5ed0f5a8fb3e6b9c041b6df749e3dfdfe03e8
alexander "sandy" gilfillan <alexander.l.gilfillan@gmail.com>
2016-04-20 10:43:58 -0700

    properly scrapes data into our very own 'markers' object.

--- routes/index.js
+++ routes/index.js
@@ -3,25 +3,29 @@ var router = express.Router();
 var rp = require('request-promise');
 
 var userZip = 98122;
-var radius = 20;
+var radius = 50;
 var userLat;
 var userLon;
 var key = '7731a58403d7c1d1d331c3e714c349';
 var markers = [];
 var venueName;
 
-rp({uri:'https://api.meetup.com/2/open_events?key='+key +'&zip='+userZip.toString()+'&radius'+radius.toString()+'&status=upcoming'
+rp({uri:'https://api.meetup.com/2/open_events?key='+key +'&zip='+userZip.toString()+'&radius='+radius.toString()+'&status=upcoming'
 }).then(function(data) {
   var parseData = (JSON.parse(data));
   for(var i = 0; i < parseData.results.length; i++) {
-    // if(parseData.results[i].hasOwnProperty('venue')) {
-      console.log(parseData.results[i].hasOwnProperty('venue'));
-    //   parseData.results[i].venue.name = " ";
-    // }
-    // markers.push({
-    //   name: parseData.results[i].venue.name,
-    // })
+    if(parseData.results[i].hasOwnProperty('venue')) {
+      markers.push({
+        name: parseData.results[i].venue.name,
+        description: parseData.results[i].venue.zip,
+        rsvpCount: parseData.results[i].yes_rsvp_count,
+        lat: parseData.results[i].venue.lat,
+        lon: parseData.results[i].venue.lon,
+
+      })
+    }
   }
+  console.log(markers);
 });
 // console.log(markers);
 /* GET home page. */
@@ -31,11 +35,6 @@ router.get('/', function(req, res, next) {
 });
 
 
-    //     description: parseData.results[i].venue.zip,
-    //     rsvpCount: parseData.results[i].yes_rsvp_count,
-    //     lat: parseData.results[i].venue.lat,
-    //     lon: parseData.results[i].venue.lon,
-    // }
 
 
 
